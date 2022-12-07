b01363d26b3a0586906d8cc5535a82646fcb7e88
Dylan Budnick <dylanjbudnick@gmail.com>
2022-12-07 00:19:14 -0500

    terminal input/output working
    This push adds a new script that allows users to interact with a chatbot using voice input and output. The chatbot is implemented using the revChatGPT library and uses the Hugging Face Whisper-Tiny model to generate responses. The script uses the record and speak libraries to handle the speech recognition and text-to-speech conversion. The API key for the Hugging Face API is loaded from a .env file. The script allows users to enable or disable speech output, and continues the conversation until the user decides to exit.

--- README.md
+++ README.md
@@ -4,29 +4,37 @@ The Whisper Chatbot is a simple voice-based chatbot that uses the ChatGPT model 
 
 ## Requirements
 
-- Python 3.6 or higher
-- PyAudio
-- Colorama
-- HuggingFace API key
-- homebrew for OSX
-- brew install pyaudio may be required
-- pip install pyaudio may be required
-- playwright may be required after chatgpt install
+The following packages are required to run the Whisper Chatbot:
 
-
-The Whisper Chatbot uses the [HuggingFace Whisper-Tiny API](https://api-inference.huggingface.co/models/openai/whisper-tiny) for speech-to-text transcription.
-
-- Follow the instructions here to configure ChatGPT with Playwright:  https://github.com/mmabrouk/chatgpt-wrapper
+    Python 3.6 or higher
+    PyAudio
+    Colorama
+    HuggingFace API key
+    homebrew for OSX (if using OSX)
+    brew install pyaudio (if using OSX)
+    pip install pyaudio (if using OSX)
+    playwright (if using OSX)
 
 ## Usage
 
 To use the Whisper Chatbot, you will need to obtain an API key from HuggingFace. To do this, go to the [HuggingFace website](https://huggingface.co/) and sign up for an account. Once you have an account, go to the "Models" section and search for the "Whisper-Tiny" model. Click on the model to see the details, and then click on the "Create a new model" button. Give your model a name, and click on the "Create" button. This will generate an API key that you can use with the Whisper Chatbot.
 
-Once you have an API key, you can add it to the script by replacing the `xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx` placeholder in the following line of code with your actual API key:
+In the latest version of the code, the API key is no longer hardcoded in the script and instead is stored in a config.json file. This allows for easier management of the API key, as well as the ability to use different API keys for different environments (e.g. development and production).
 
-```python
-headers = {"Authorization": "Bearer xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}
-```
+To use your API key with the Whisper Chatbot, you will need to add it to the config.json file. The file should look like this:
+
+{
+  "api_key": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+}
+Simply replace the xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx placeholder with your actual API key, and save the file. The Whisper Chatbot will then use this API key to access the HuggingFace Whisper-Tiny API.
+
+Note that you will need to obtain an API key from HuggingFace as described in the Usage section of the README.
+
+Including a screenshot section:
+
+Whisper Chatbot screenshot
+
+The goal of this code is to create a voice-based chatbot that uses the ChatGPT model to generate responses to user inputs. The chatbot uses the HuggingFace Whisper-Tiny API for speech-to-text transcription, and allows users to interact with the chatbot by speaking to it. The chatbot will transcribe the user's speech and generate a response based on the ChatGPT model.
 
 We recommend using a virtual environment to manage the dependencies for the Whisper Chatbot. To create a virtual environment and install the necessary packages, you can use the following commands:
 
@@ -49,26 +57,28 @@ This will start the Whisper Chatbot, and you can start talking to it by pressing
 ## Example Output
 
 ``` python Welcome to the Whisper Chatbot! Press Ctrl+C to exit.
-* recording for 2 seconds
-* done recording
-Starting ... Done
-You:  Apple.
 
-ChatGPT: Apple is a multinational technology company that designs, develops, and sells consumer electronics, computer software, and online services. The company's best-known hardware products include the iPhone, iPad, and Mac personal computers. Apple's software includes the macOS and iOS operating systems, the iTunes media player, and the Safari web browser. The company also offers online services such as the iCloud cloud storage service and the Apple Music streaming service.
+Logging in...
+Do you want to enable speech? (y/n) y
+Say something to chatbot! I am listening right now!
 
-##################################################
+Recognized in 2.646164894104004 seconds
+Activated
 
-Success!
+First prompt understood and sent
 
-Read the above response...
-Enter a time for the next window of wav recording, that is an even number and press enter to continue when ready; to exit, press Ctrl+C
+Recognized:  What's the capital of Spain?
+Conversation ID: 
+Parent ID: 
+Response received
 
-Enter the duration in seconds. And hit enter twice. Type n or 'exit' to quit:  
+###############
 
-```
+Response: The capital of Spain is Madrid.
 
+###############
 
+Response spoken
 
-## License
-
-This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
+Do you want to continue (y/n)? y
+Please speak your additional prompt to chatbot...
--- whisper_chatbot.py
+++ whisper_chatbot.py
@@ -1,146 +1,61 @@
-import wave
-import pyaudio
-import requests
+from revChatGPT.revChatGPT import Chatbot
 import json
-import threading
-import time
-import keyboard
 import colorama
+from record import SpeechRecognizer
+from speak import Speak
 from dotenv import load_dotenv
-from dotenv import dotenv_values
+import os
 
-config = dotenv_values(".env")  # config = {"TOKEN": "foo", "EMAIL": "foo@example.org"}
+# Load the API key from the .env file
+load_dotenv()
+API_KEY = os.environ.get("API_KEY")
 
-TOKEN = config["TOKEN"]
+#### Using whisper-tiny.en, you can select a whisper model from the list of models here: https://huggingface.co/models?filter=whisper ####
 
-
-# Function that continuously checks for changes to the audio wav file
-def check_for_changes():
-  # Get the current version of the audio wav file
-  current_version = get_current_version_of_audio_wav("output.wav")
-  
-  # Continuously check for changes to the audio wav file
-  while True:
-    # Check if the audio wav file has been updated
-    updated_version = get_current_version_of_audio_wav("output.wav")
-    if updated_version != current_version:
-      # If the audio wav file has been updated, perform the desired check
-      current_version = updated_version
-      check_audio_wav(updated_version)
-    
-    # Sleep for a short amount of time before checking again
-    time.sleep(0.1)
-
-# Start the thread that checks for changes to the audio wav file
-import requests
-
-API_URL = "https://api-inference.huggingface.co/models/openai/whisper-tiny"
-headers = {"Authorization": "Bearer " + TOKEN}
-
-def query(filename):
-    with open(filename, "rb") as f:
-        data = f.read()
-    response = requests.request("POST", API_URL, headers=headers, data=data)
-    return json.loads(response.content.decode("utf-8"))
-
-output = query("output.wav")
+### Use your API KEY to replace the xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx below ###
 
 
-def transcribe_recording(filename):
-    # Define a function to query the speech-to-text model with the given file
-    def query(filename):
-        with open(filename, "rb") as f:
-            data = f.read()
-        response = requests.request("POST", API_URL, headers=headers, data=data)
-        return json.loads(response.content.decode("utf-8"))
-        # Return the transcription result
-    return query(filename)["text"]
-
-    # Query the speech-to-text model with the recording
-    output = query(filename)
-
-    # Print the transcription result
-    print(output)
-
-    # Return the transcription result
-    return output
-
-def record(chunk, format, channels, rate, record_seconds, wave_output_filename):
-    p = pyaudio.PyAudio()
-
-    stream = p.open(format=format,
-                    channels=channels,
-                    rate=rate,
-                    input=True,
-                    frames_per_buffer=chunk)
-
-    print(colorama.Fore.BLUE + "* recording for " +str(record_seconds) + " seconds" + colorama.Fore.RESET)
+API_URL = "https://api-inference.huggingface.co/models/openai/whisper-tiny.en"
+headers = {"Authorization": "Bearer xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"}
 
 
-    frames = []
+config = json.load(open("config.json"))
+chatbot = Chatbot(config, conversation_id=None)
+chatbot.refresh_session() # You need to log in on the first run
 
-    for i in range(0, int(rate / chunk * record_seconds)):
-        data = stream.read(chunk)
-        frames.append(data)
-       
+listen = SpeechRecognizer()
 
-    print(colorama.Fore.GREEN + "* done recording")
+speak = Speak()
 
-    stream.stop_stream()
-    stream.close()
-    p.terminate()
+speech = bool(input("Do you want to enable speech? (y/n) ") == "n")
 
-    wf = wave.open(wave_output_filename, 'wb')
-    wf.setnchannels(channels)
-    wf.setsampwidth(p.get_sample_size(format))
-    wf.setframerate(rate)
-    wf.writeframes(b''.join(frames))
-    wf.close()
+def get_chat_response(self, prompt, output="text"):
+    return {'message':message, 'conversation_id':self.conversation_id, 'parent_id':self.parent_id}
 
-print("Welcome to the Whisper Chatbot! Press Ctrl+C to exit.")
-
-record(1024, pyaudio.paInt16, 2, 44100, 2, "output.wav")
-transcribe_recording("output.wav")
-
-output = query("output.wav")
-
-#return output["text"]
-
-
-from chatgpt_wrapper import ChatGPT
-
-chatbot = ChatGPT()
-
-def get_response(message):
-    return chatbot.ask(message)
-
+print(colorama.Fore.RED + "Say something to chatbot! I am listening right now!\n" + colorama.Fore.RESET)
 while True:
-    output = query("output.wav")
-    inp = input(colorama.Fore.CYAN + "You: " + str(output['text']))
-    response = chatbot.ask(inp + (output['text']))
-    #parse the response on nice lines
-    print(colorama.Fore.YELLOW + "\nChatGPT: " + response + "\n" + colorama.Style.RESET_ALL)
-    print(colorama.Fore.LIGHTMAGENTA_EX+ "##################################################" + "\n" + colorama.Style.RESET_ALL)
-
- 
-    #await response
-    #await asyncio.sleep(0.1)
-    print(colorama.Fore.GREEN + "Success!" + "\n"+ colorama.Style.RESET_ALL)
-    print(colorama.Fore.RED + "Read the above response..." + "\n" + "Enter a time for the next window of wav recording, that is an even number and press enter to continue when ready; to exit, press Ctrl+C" + "\n" +colorama.Style.RESET_ALL)
-    duration = input("Enter the duration in seconds. And hit enter twice. Type " + colorama.Fore.RED + 'n' + colorama.Style.RESET_ALL + " or " + colorama.Fore.BLUE + "'exit' " + colorama.Style.RESET_ALL + "to quit: ")
-    duration = int(duration)
-    choice = input()
-    if choice == 'n' or choice == 'exit':
-        break
-    else:
-        record(1024, pyaudio.paInt16, 2, 44100, duration, "output.wav")
-        transcribe_recording("output.wav")
-
-
-
-
-
-# if __name__ == "__main__":
-#     interface = gr.Interface(fn=get_response, inputs=gr.inputs.Textbox(
-#         lines=5, label="You"), outputs=gr.outputs.Textbox(label="chatGPT"))
-#     interface.launch()
+    # Listen for user input
+    prompt = listen.listen()
+    print(colorama.Fore.RED + "Activated\n" + colorama.Fore.RESET)
+    print(colorama.Fore.GREEN + "First prompt understood and sent\n" + colorama.Fore.RESET)
+    print(f"Recognized: {prompt}")
+    print (f"Conversation ID: {chatbot.conversation_id}")
+    print (f"Parent ID: {chatbot.parent_id}")
+    resp = chatbot.get_chat_response(prompt)
+    response = resp['message']
+    print(colorama.Fore.MAGENTA + "Response received\n" + colorama.Fore.RESET)
+    print(colorama.Fore.GREEN + "###############\n" + colorama.Fore.RESET)
+    print(colorama.Fore.YELLOW + "Response: " + response + "\n" + colorama.Fore.RESET)
+    print(colorama.Fore.GREEN + "###############\n" + colorama.Fore.RESET)
+    # Speak the response if the user has enabled it with
+    speak.speak(response)
+    print(colorama.Fore.GREEN + "Response spoken\n" + colorama.Fore.RESET)
+    # Ask the user if they want to continue
+    while True:
+        user_input = input("Do you want to continue (y/n)? ")
+        if user_input == "y":
+            print(colorama.Fore.CYAN + "Please speak your additional prompt to chatbot..." + colorama.Fore.RESET)
+            break
+        elif user_input == "n":
+            print("Exiting program")
+            exit()
